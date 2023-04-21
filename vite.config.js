8216b7e9390824d3a071f4f79afb06df37a642c4
lenin cruz mayo <40728531+lenin-developer@users.noreply.github.com>
2023-04-20 19:05:44 -0600

    build: postCSS: #add

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import postcssPresetEnv from "postcss-preset-env"; // importar esto
 
-export default ({ mode }) => {
+export default () => {
   return defineConfig({
     plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcssPresetEnv({
+            stage: false,
+            features: {
+              "nesting-rules": true,
+              "custom-media-queries": true,
+            },
+          }),
+        ],
       },
     },
   });
